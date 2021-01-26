4bf3b3bea2fdf7a5ba81f77a3334c764a834b9c8
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-01-26 18:53:25 +0900

    edit 2011

--- src/DP/p_2011.java
+++ src/DP/p_2011.java
@@ -1,2 +1,20 @@
-package DP;public class p_2011 {
+package DP;
+
+import java.util.Scanner;
+
+//암호코드
+public class p_2011 {
+    public static void main(String[] args) {
+
+        //A~I : 1~9
+        //J~S : 10~19
+        //T~Z : 20~26
+        long n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+
+
+
+    }
 }
