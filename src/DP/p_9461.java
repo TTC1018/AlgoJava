d7f993e2fd070fc0732b6bd8dc80004d616f1708
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-01-26 15:46:09 +0900

    add 9461

--- src/DP/p_9461.java
+++ src/DP/p_9461.java
@@ -2,12 +2,35 @@ package DP;
 
 import java.util.Scanner;
 
+//파도반 수열 (정삼각형 이어붙이기)
 public class p_9461 {
     public static void main(String[] args) {
-        int n;
+        int n, max;
+        int input[];
+        long dp[] = new long[101];
+
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        input = new int[n+1];
 
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
 
+        max = 0;
+        for(int i=1; i<=n; i++){
+            input[i] = sc.nextInt();
+            max = max < input[i] ? input[i] : max;
+        }
+
+        for(int i=6; i<=max; i++){
+            dp[i] = dp[i-1] + dp[i-5];
+        }
+
+        for(int i=1; i<=n; i++){
+            System.out.println(dp[input[i]]);
+        }
     }
 }
