b8045f44e79cc5a60529ed9d307bb1e693702d5e
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-01-27 19:05:25 +0900

    create 1463

---
+++ src/DP/p_1463.java
@@ -0,0 +1,22 @@
+package DP;
+
+import java.util.Scanner;
+
+//1로 만들기 (최소 연산횟수 구하기)
+public class p_1463 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+
+        if(n%3 == 0){
+
+        }else if(n%2 == 0){
+
+        }else {
+
+        }
+
+    }
+}
