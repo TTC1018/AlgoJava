ef9b2edf467c863a531d7afc41e3be788a0ecdfa
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-01-29 18:03:02 +0900

    add 9465

---
+++ src/DP/p_9465.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.util.Scanner;
+
+//스티커
+public class p_9465 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t, n; // t = 테스트 케이스 개수, n = 열의 개수
+        t = sc.nextInt();
+        n = sc.nextInt();
+        sc.nextLine();
+
+        int dp[][] = new int[3][100001];
+        int answer[] = new int[t+1];
+
+
+        for(int i=1; i<=t; i++){
+            for(int j=1; j<=n; j++){
+
+            }
+        }
+
+    }
+}
