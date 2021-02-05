98030ffe956757bb37c874e4eac95cb3c39a9426
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-02-05 14:48:00 +0900

    solve 1520

--- src/DP/p_1520.java
+++ src/DP/p_1520.java
@@ -2,7 +2,11 @@ package DP;
 
 import java.util.Scanner;
 
+//내리막 길
 public class p_1520 {
+    static int[][] dp, arr;
+    static int m, n;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String temp;
@@ -10,56 +14,56 @@ public class p_1520 {
 
         temp = sc.nextLine();
         temp2 = temp.split(" ");
-        int m = Integer.parseInt(temp2[0]) , n = Integer.parseInt(temp2[1]);
-        int dp[][] = new int[m+2][n+2];
-        int arr[][] = new int[m+2][n+2];
+        m = Integer.parseInt(temp2[0]);
+        n = Integer.parseInt(temp2[1]);
+        dp = new int[m+1][n+1];
+        arr = new int[m+1][n+1];
 
         for(int i=1; i<=m; i++){
             temp = sc.nextLine();
             temp2 = temp.split(" ");
             for(int j=1; j<=n; j++){
                 arr[i][j] = Integer.parseInt(temp2[j-1]);
-                dp[i][j] = 0;
+                dp[i][j] = -1;
             }
         }
 
-        //padding
-        for(int i=0; i<=n; i++){
-            arr[0][i] = -1;
-            arr[m+1][i] = -1;
-            arr[i][0] = -1;
-            arr[i][n+1] = -1;
-            dp[0][i] = -1;
-            dp[m+1][i] = -1;
-            dp[i][0] = -1;
-            dp[i][n+1] = -1;
-        }
-        dp[1][1] = 1;
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(i == 1 && j == 1) continue;
-
-                if(arr[i][j] < arr[i][j-1]){
-                    if(dp[i][j-1] != -1) dp[i][j] += dp[i][j-1]; //좌
-                }
-                if(arr[i][j] < arr[i][j+1]){
-                    if(dp[i][j+1] != -1) dp[i][j] += dp[i][j+1]; //우
-                }
-                if(arr[i][j] < arr[i-1][j]){
-                    if(dp[i-1][j] != -1) dp[i][j] += dp[i-1][j]; //상
-                }
-                if(arr[i][j] < arr[i+1][j]){
-                    if(dp[i+1][j] != -1) dp[i][j] += dp[i+1][j]; //하
-                }
-            }
-       }
+        if(m == 1 && n == 1) System.out.println(1);
+        else System.out.println(solve_method(1, 1));
 
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
-                System.out.print(dp[i][j]+ " ");
+                System.out.printf("%3d", dp[i][j]);
             }
             System.out.println();
         }
-        System.out.println(dp[m][n]);
+    }
+
+    //메소드로 배열의 반복된 합을 표현한다
+    private static int solve_method(int x, int y){
+        if(x == m && y == n){
+            dp[x][y] = 1;
+            return 1;
+        }
+        if(dp[x][y] == -1){
+            dp[x][y] = 0;
+
+            if(x > 1 && arr[x][y] > arr[x-1][y]){
+                dp[x][y] += solve_method(x-1, y);
+            }
+
+            if(x < m && arr[x][y] > arr[x+1][y]){
+                dp[x][y] += solve_method(x+1, y);
+            }
+
+            if(y > 1 && arr[x][y] > arr[x][y-1]){
+                dp[x][y] += solve_method(x, y-1);
+            }
+
+            if(y < n && arr[x][y] > arr[x][y+1]){
+                dp[x][y] += solve_method(x, y+1);
+            }
+        }
+        return dp[x][y];
     }
 }
