6b61d9b9366bfc37bab1f365617a632831859da0
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-02-15 16:43:47 +0900

    solve 10799

--- src/Stack/p_10799.java
+++ src/Stack/p_10799.java
@@ -13,13 +13,15 @@ public class p_10799 {
         Stack<String> stack = new Stack<>();
         for(int i=0; i<temp.length; i++) stack.push(temp[i]);
 
-        Stack<String> stack2 = new Stack<>();
+        Stack<String> stack2 = new Stack<>(); //우괄호를 담는 stack
+        Stack<Integer> stackL = new Stack<>(); //누적 laser 담는 stack
+
         int curLasers = 0;
-        int maxLasers = 0;
+        int stackedL = 0;
         int cut = 0;
 
         //큰 ()가 유지되다가 중간에 새로운 ()가 유지될 때의 경우까지 고려하는 것이 필요
-        while(stack.size() >= 2){
+        while(stack.size() != 0){
             String top = stack.peek();
             if(top.equals(")")){
                 stack2.push(stack.pop());
@@ -27,37 +29,33 @@ public class p_10799 {
                 if(stack.peek().equals("(")){//레이저 생성되는 경우
                     if(stack2.size() == 1){ //바깥 괄호 없는 경우
                         curLasers = 0;
-                        maxLasers = 0;
                         stack.pop();
                         stack2.pop();
                     }else{
                         curLasers++;
                         stack.pop();
                         stack2.pop();
+                        if(stack.size() >= 3 && stack.peek().equals(")")){
+                            stackL.push(stackL.pop() + curLasers);
+                            curLasers = 0;
+                        }
                     }
                 }
                 else{ //또 ")"가 있을 때
-                    maxLasers += curLasers;
-                    curLasers = 0;
+                    stackL.push(curLasers);
                 }
             }
             else if(top.equals("(")){
-                if(curLasers != 0){
-                    if(stack2.size() != 1) maxLasers += curLasers;
-                    cut += (curLasers+1);
-                    curLasers = 0;
-                    stack.pop();
-                    stack2.pop();
-                }
-                else{
-                    cut += (maxLasers+1);
-                    stack.pop();
-                    stack2.pop();
-                }
-            }
-            System.out.println("curL : " + curLasers + ", maxL : " + maxLasers + ", cut : " + cut);
-        }
+                stackedL = stackL.pop() + curLasers;
+                cut += (stackedL+1);
 
+                if(stackL.size() != 0) stackL.push(stackL.pop() + stackedL);
+
+                curLasers = 0;
+                stack.pop();
+                stack2.pop();
+            }
+        }
         bw.write(Integer.toString(cut));
         bw.flush();
     }
